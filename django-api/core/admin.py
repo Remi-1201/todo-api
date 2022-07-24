904e0cb386ce304b245b40876aeb37c41cfb0f2c
remi-1201 <remi.nkjm@gmail.com>
2022-07-24 19:08:09 +0900

    3.8 管理画面にモデルを登録

--- django-api/core/admin.py
+++ django-api/core/admin.py
@@ -1,6 +1,16 @@
+# 3.8 管理画面にモデルを登録
+# - 作成した User モデルを管理画面で確認できるよう、admin.py に変更を加えます
 from django.contrib import admin
-
+"""
+BaseUserAdmin = デフォルトの UserAdmin を BaseUserAdmin としてインポートし、
+それを継承することでカスタマイズします
+"""
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+"""
+gettext as _ = gettext は多言語対応のために利用されます。
+慣習的に as _ としてインポートするみたいです。
+今回、言語設定に関してはデフォルトの英語のまま進める。
+"""
 from django.utils.translation import gettext as _
 
 from core import models
